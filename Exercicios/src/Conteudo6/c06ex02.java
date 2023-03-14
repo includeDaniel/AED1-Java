ac7ca7d3febfc5f70e83204e207777e692868ac4
Daniel Silva <danielzcomputacao@gmail.com>
2023-03-14 14:27:19 -0300

    New Exercises done

---
+++ Exercicios/src/Conteudo6/c06ex02.java
@@ -0,0 +1,20 @@
+package Conteudo6;
+
+import java.util.Scanner;
+
+public class c06ex02 {
+    public static void main(String[] args) {
+        Scanner teclado = new Scanner(System.in);
+        System.out.print("Digite o número de poluentes: ");
+        double poluentes = teclado.nextDouble();
+        if(poluentes <= 1500) {
+            System.out.print("Isento de multa");
+        } else if( poluentes > 1500 && poluentes < 3500) {
+            System.out.print("Taxa de R$3.000,00");
+        } else {
+            double acima3500 = (5000 * poluentes);
+            System.out.print("Taxa de: " + acima3500);
+        }
+        teclado.close();
+    }
+}
---
+++ Exercicios/src/Conteudo6/c06ex04.java
@@ -0,0 +1,7 @@
+package Conteudo6;
+
+public class c06ex04 {
+    public static void main(String[] args) {
+        
+    }
+}
