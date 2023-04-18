f10d76d6077b034dafe4bfe1b195dd1a38f71f46
Daniel Silva <danielzcomputacao@gmail.com>
2023-04-17 22:28:49 -0300

    challenge done

--- Exercicios/src/Conteudo9/c09ex13.java
+++ Exercicios/src/Conteudo9/c09ex13.java
@@ -1,17 +1,24 @@
 package Conteudo9;
 
+import javax.sql.rowset.serial.SerialException;
+
 public class c09ex13 {
     public static void main(String[] args) {
-        double num, den, serie = 0;
-        double soma = 1;
+        double num = 0, den, serie = 0, fatorial = 1;
+        int contador = 4;
         for (int i = 1; i <= 10; i++) {
+            fatorial *= i;
 
-            num = i * i * i;
+            num = fatorial;
 
             den = Math.pow(2, i) - 1;
-            serie += num / den;
+            
+            if (i % 2 == 0) {
+                serie -= num / den;
+            } else {
+                serie += num / den;
+            }
         }
-
-        System.out.print(serie);
+        System.out.println(serie);
     }
 }
