e1e06516b5e33709408f6cbcaa5214ca721f2a44
Daniel Silva <danielzcomputacao@gmail.com>
2023-04-21 18:02:52 -0300

    c10ex07 done

---
+++ Exercicios/src/Conteudo10/c10ex07.java
@@ -0,0 +1,30 @@
+package Conteudo10;
+
+import java.util.Scanner;
+
+public class c10ex07 {
+    public static void main(String[] args) {
+        double populacao, infectados = 1, populacaoinfectada = 0, anosDecimal, divisao = 0, anosConta = 0,
+                mesesConta = 0,
+                diasConta;
+        int contDia = 0;
+        Scanner console = new Scanner(System.in);
+        System.out.print("Digite a população: ");
+        populacao = console.nextDouble();
+        do {
+            infectados += (infectados * 0.003);
+            contDia++;
+
+        } while (infectados <= populacao);
+        anosConta = contDia / 365;
+        mesesConta = (contDia - (anosConta * 365)) / 30;
+        int meses = (int) mesesConta;
+        diasConta = contDia - (anosConta * 365) - (meses * 30);
+        int dias = (int) diasConta;
+        int anos = (int) anosConta;
+
+        System.out.println("Demorará " + anos + " anos " + meses + " meses e " + dias
+                + " dias para todas as pessoas serem infectadas");
+        console.close();
+    }
+}
