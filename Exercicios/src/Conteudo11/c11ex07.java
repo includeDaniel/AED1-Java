e2dd67ac823476f4e1c7c0ba59e86db4bc687fdc
Daniel Silva <danielzcomputacao@gmail.com>
2023-04-29 19:07:36 -0300

    c11ex07.java

---
+++ Exercicios/src/Conteudo11/c11ex07.java
@@ -0,0 +1,33 @@
+package Conteudo11;
+
+import java.util.Scanner;
+
+public class c11ex07 {
+    public static void main(String[] args) {
+        int cont = 0;
+        double temperatura, maiorTemperatura = 0, menorTemperatura = 0, temperaturaSoma = 0, media;
+        Scanner console = new Scanner(System.in);
+
+        for (int i = 1; i <= 5; i++) {
+            System.out.print("Digite a temperatura: ");
+            temperatura = console.nextDouble();
+
+            if (temperatura > maiorTemperatura) {
+                maiorTemperatura = temperatura;
+                cont++;
+            }
+            if (i == 1 || temperatura < menorTemperatura) {
+                menorTemperatura = temperatura;
+            }
+
+            temperaturaSoma += temperatura;
+        }
+        console.close();
+        media = temperaturaSoma / 5.0;
+        System.out.println();
+        System.out.println("A menor temperatura registrada: " + menorTemperatura);
+        System.out.println("A maior temperatura registrada: " + maiorTemperatura);
+        System.out.println("Média das temperaturas registrads: " + media);
+        System.out.println("A quantidade de dias que ocorreu a maior temperatura: " + cont);
+    }
+}
