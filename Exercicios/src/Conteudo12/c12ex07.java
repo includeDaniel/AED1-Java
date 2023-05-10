3d93e613344a0815a7e8791f3e66116a6c0193c9
Daniel Silva <danielzcomputacao@gmail.com>
2023-05-10 20:25:06 -0300

    c12ex07 done

---
+++ Exercicios/src/Conteudo12/c12ex07.java
@@ -0,0 +1,36 @@
+package Conteudo12;
+
+import java.util.Scanner;
+
+public class c12ex07 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String names[] = new String[5], conceito;
+        double nota[] = new double[5];
+        for (int i = 0; i < names.length; i++) {
+            System.out.print("Digite o nome do aluno: ");
+            names[i] = console.nextLine();
+            System.out.print("Digite a nota do aluno: ");
+            nota[i] = console.nextDouble();
+            console.nextLine();
+        }
+        for (int i = 0; i < names.length; i++) {
+            System.out.print(names[i]);
+
+            if (nota[i] <= 30) {
+                conceito = "D";
+            } else if (nota[i] > 30 && nota[i] <= 60) {
+                conceito = "C";
+            } else if (nota[i] > 60 && nota[i] <= 80) {
+                conceito = "B";
+            } else {
+                conceito = "A";
+            }
+
+            System.out.print(", Conceito: " + conceito);
+            System.out.println();
+        }
+
+        console.close();
+    }
+}
