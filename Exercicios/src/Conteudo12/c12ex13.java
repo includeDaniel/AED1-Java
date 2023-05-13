2f86be711760b38b1e7af334a7a15c908ae2949f
Daniel Silva <danielzcomputacao@gmail.com>
2023-05-13 18:52:36 -0300

    c12ex13 done

---
+++ Exercicios/src/Conteudo12/c12ex13.java
@@ -0,0 +1,28 @@
+package Conteudo12;
+
+import java.util.Scanner;
+
+public class c12ex13 {
+    public static void main(String[] args) {
+        Scanner console = new Scanner(System.in);
+        String meses[] = new String[12], temp;
+        for (int i = 0; i < meses.length; i++) {
+            System.out.print("Digite os meses 1 por 1: ");
+            meses[i] = console.nextLine();
+        }
+        for (int i = 0; i < meses.length - 1; i++) {
+            for (int j = 0; j < meses.length - 1; j++) {
+                if (meses[j].compareToIgnoreCase(meses[j + 1]) > 0) {
+                    temp = meses[j];
+                    meses[j] = meses[j + 1];
+                    meses[j + 1] = temp;
+                }
+
+            }
+        }
+
+        for (int i = 0; i < meses.length; i++) {
+            System.out.println(meses[i]);
+        }
+    }
+}
